70b6f0f0916f36b324a10f2c04593e198047e85a
Victor Johnson <vjohnson22@gmail.com>
2019-08-19 14:52:11 -0400

    index created, with routes added

---
+++ db/connection.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+let mongoURI = "";
+
+if (process.env.NODE_ENV === "production") {
+    mongoURI = process.env.DB_URL;
+  } else {
+    mongoURI = "mongodb://localhost/basketball";
+  }
+
+  mongoose
+  .connect(mongoURI, { useNewUrlParser: true })
+  .then(instance =>
+    console.log(`Connected to db: ${instance.connections[0].name}`)
+  )
+  .catch(error => console.log("Connection failed!", error));
+
+mongoose.Promise = Promise
+
+module.exports = mongoose
---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.use('/', require('./routes/player'))
+app.use('/searched', require('./routes/searched'))    
+
+
+app.set("port", process.env.PORT || 3000)
+
+app.listen(app.get("port"), () => {
+  console.log(`✅ PORT: ${app.get("port")} 🌟`);
+});
