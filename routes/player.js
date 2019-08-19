a8e0d11fa5079789505db0f5d1dce133ef65615f
Victor Johnson <vjohnson22@gmail.com>
2019-08-19 15:07:19 -0400

    player router done, next add controller, then searched route/controller

---
+++ routes/player.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+const playerController = require('../controllers/player')
+
+router.get('/', playerController.index)
+router.get('/:name', playerController.showName)
+router.get('/id/:id', playerController.showId)
+router.post('/', playerController.create)
+router.patch('/', playerController.update)
+router.delete('/', playerController.destroy)
+
+module.exports = router
