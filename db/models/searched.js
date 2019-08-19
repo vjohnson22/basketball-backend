cb7bdd94dea0f7e5f4dca789fddf40b3df7f9908
Victor Johnson <vjohnson22@gmail.com>
2019-08-19 15:02:13 -0400

    searched model created

---
+++ db/models/searched.js
@@ -0,0 +1,12 @@
+const mongoose = require('../connection')
+const Schema = mongoose.Schema
+
+const SearchedSchema = new Schema ({
+    name: String,
+    playerId: {
+        type: Schema.Types.ObjectId,
+        ref: "Player"
+    }
+})
+
+module.exports = mongoose.model("Player", PlayerSchema)
