114280288c4bc79967b8fe4798b8869441be9d94
Victor Johnson <vjohnson22@gmail.com>
2019-08-19 14:57:28 -0400

    player model done

---
+++ db/models/player.js
@@ -0,0 +1,15 @@
+const mongoose = require('../connection')
+const Schema = mongoose.Schema
+
+const PlayerSchema = new Schema ({
+    name: String,
+    ppg: Number,
+    apg: Number,
+    rpg: Number,
+    spg: Number,
+    bpg: Number,
+    image: String,
+    video: String
+})
+
+module.exports = mongoose.model("Player", PlayerSchema)
